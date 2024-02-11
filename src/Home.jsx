e8de072f777bd5fd9f50ed2b98b408cc469f6499
rocky <ganjianil@outlook.com>
2024-02-11 17:42:10 +0530

    m

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,49 +12,14 @@ const Home = () => {
           <img className="img" src={image} alt="image"></img>
         </div>
         <div className="content">
-          <p>
-            To coustomizie your own website u can design your website and build
-            the things u need whant u want
-            <br /> 1.For dynamic website its free to coustomize your own things
-            what u need u can
-            <br /> 2.for static website also its free to coustomiz lets check
-          </p>
+            <p>To coustomizie your own website u can design your website and build the things u need whant u want 
+               <br/> 1.For dynamic website its free to coustomize your own things what u need u can 
+               <br/> 2.for static website also its free to coustomiz lets check 
+            </p>
+
         </div>
       </div>
-      <div className="header">
-        <h1>WEBSITE</h1>
-      </div>
-      <div className="webdesign">
-        <div className="image">
-          <img className="img" src={image} alt="image"></img>
-        </div>
-        <div className="content">
-          <p>
-            To coustomizie your own website u can design your website and build
-            the things u need whant u want
-            <br /> 1.For dynamic website its free to coustomize your own things
-            what u need u can
-            <br /> 2.for static website also its free to coustomiz lets check
-          </p>
-        </div>
-      </div>
-      <div className="header">
-        <h1>WEBSITE</h1>
-      </div>
-      <div className="webdesign">
-        <div className="image">
-          <img className="img" src={image} alt="image"></img>
-        </div>
-        <div className="content">
-          <p>
-            To coustomizie your own website u can design your website and build
-            the things u need whant u want
-            <br /> 1.For dynamic website its free to coustomize your own things
-            what u need u can
-            <br /> 2.for static website also its free to coustomiz lets check
-          </p>
-        </div>
-      </div>
+      
     </div>
   );
 };
