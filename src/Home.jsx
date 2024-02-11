d7f663ec4709b3389a821c4259b0e5f9eebff97e
rocky <ganjianil@outlook.com>
2024-02-11 17:16:37 +0530

    mee

---
+++ src/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import image from "./OIP.jpg"
+
+const Home = () => {
+  return (
+    <div className="web">
+      <div className="header">
+        <h1>WEBSITE</h1>
+      </div>
+      <div className="webdesign">
+        <div className="image">
+          <img className="img" src={image} alt="image"></img>
+        </div>
+        <div className="content">
+          <p>
+            To coustomizie your own website u can design your website and build
+            the things u need whant u want
+            <br /> 1.For dynamic website its free to coustomize your own things
+            what u need u can
+            <br /> 2.for static website also its free to coustomiz lets check
+          </p>
+        </div>
+      </div>
+      <div className="header">
+        <h1>WEBSITE</h1>
+      </div>
+      <div className="webdesign">
+        <div className="image">
+          <img className="img" src={image} alt="image"></img>
+        </div>
+        <div className="content">
+          <p>
+            To coustomizie your own website u can design your website and build
+            the things u need whant u want
+            <br /> 1.For dynamic website its free to coustomize your own things
+            what u need u can
+            <br /> 2.for static website also its free to coustomiz lets check
+          </p>
+        </div>
+      </div>
+      <div className="header">
+        <h1>WEBSITE</h1>
+      </div>
+      <div className="webdesign">
+        <div className="image">
+          <img className="img" src={image} alt="image"></img>
+        </div>
+        <div className="content">
+          <p>
+            To coustomizie your own website u can design your website and build
+            the things u need whant u want
+            <br /> 1.For dynamic website its free to coustomize your own things
+            what u need u can
+            <br /> 2.for static website also its free to coustomiz lets check
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
